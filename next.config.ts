7575b522bc339402f82663cb94f5328faab8e59b
LJaeson <breakmodejason@gmail.com>
2026-04-06 23:55:01 +1000

    config the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,39 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          destination: '/Jaeson_Liang_academic_statement.pdf',
+        },
+      ],
+    };
+  },
+  async headers() {
+    return [
+      {
+        source: '/', 
+        headers: [
+          {
+            key: 'Content-Disposition',
+            value: 'attachment; filename="Jaeson_Liang_academic_statement.pdf"',
+          },
+        ],
+      },
+      {
+        source: '/Jaeson_Liang_academic_statement.pdf', 
+        headers: [
+          {
+            key: 'Content-Disposition',
+            value: 'attachment; filename="Jaeson_Liang_academic_statement.pdf"',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
